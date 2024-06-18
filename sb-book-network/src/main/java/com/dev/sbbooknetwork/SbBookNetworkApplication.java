babb2e812bc7a58b2307be4bbb10b94aa8aacd2d
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-06-18 14:19:16 +0700

    Update

---
+++ sb-book-network/src/main/java/com/dev/sbbooknetwork/SbBookNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sbbooknetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBookNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBookNetworkApplication.class, args);
+    }
+
+}
---
+++ sb-book-network/src/test/java/com/dev/sbbooknetwork/SbBookNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sbbooknetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBookNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
